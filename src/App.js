856fcad8cdd6dd5edbde3466fafbb2786218a954
Thomas Yirgetu <tom.dibora@gmail.com>
2023-07-30 08:54:07 -0700

    fourth commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
 import './App.css';
 import Search from './components/Search';
+import { useState } from 'react';
+const ENDPOINT = "https://api.dictionaryapi.dev/api/v2/entries/en/";
 
 function App() {
+  const [searchResult, setSearchResult]   = useState([]);
+  async function handleSearch(searchWord) {
+    const url = `${ENDPOINT}${searchWord}`;
+    const response = await fetch(url)
+    const json = await response.json()
+    setSearchResult(json[0])
+    
+   }
+  const nouns = searchResult?.meanings?.find((result) => result.partOfSpeech === "noun");
+  const verbs = searchResult?.meanings?.find((result) => result.partOfSpeech === "verb");
+  console.log(searchResult)
   return (
     <div className="App">
-      <Search />
+      <Search onSearch={handleSearch}/>
+      <div>
+        <div>
+        <h1>{searchResult?.word}</h1>
+        <h2>noun</h2>
+        <p>Meaning</p>
+        {nouns?.definitions.map((result) => {
+            return (
+                <li key={result.definition}>{result.definition}</li>
+            )   
+        }
+        )}
+        <p>Synonyms</p>{" "} {nouns?.synonyms.map((result) => {
+            return (
+                <li key={result}>{result}</li>
+            )   
+        }
+        )}
+        </div>
+        <div>
+        <h2>Verb</h2>
+        <p>Meaning</p>
+        {verbs?.definitions.map((result) => {
+            return (
+                <li key={result.definition}>{result.definition}</li>
+            )   
+        }
+        )}
+        </div>
+        <p>Source</p>{searchResult.sourceUrls}
+      </div>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,45 +1,27 @@
 import React, { useId, useState } from 'react'
 
-function Search() {
- const [SearchWord, setSearchWord]   = useState("");
- const [SearchResult, setSearchResult]   = useState([]);
+function Search({ onSearch }) {
+ const [searchWord, setSearchWord]   = useState("");
  const id = useId();
  const searchid = `${id}-search`
  
-   async function handleSearch(e) {
-    e.preventDefault()
-    const ENDPOINT = "https://api.dictionaryapi.dev/api/v2/entries/en/";
-    const url = `${ENDPOINT}${SearchWord}`;
-    const response = await fetch(url)
-    const json = await response.json()
-    setSearchResult(SearchResult.push(...json[0].meanings))
-    console.log(json[0].meanings[0].definitions[0].definition)
-    console.log(json)
-    console.log(SearchResult)
-    console.log(Array.isArray(SearchResult))
-    
-    return json
-   }
- 
+function handleSearch(e) {
+  e.preventDefault()
+  onSearch(searchWord)
+}
   return (
     <div>
       <form onSubmit={handleSearch}>
         <input 
         id={searchid}
-        value={SearchWord}
+        value={searchWord}
         type="search"
         onChange={(event) => {
          setSearchWord(event.target.value)
         }}
         />
       </form>
-      <div>
-        {SearchWord}
-        {SearchResult?.map((result) => {
-            return <li>{result.partOfSpeech}</li>;
-        }
-        )}
-      </div>
+      
     </div>
   )
 }
